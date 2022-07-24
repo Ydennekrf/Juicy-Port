9b701e2a2f7b55c661dbe482807b55479c86ba6d
ydennekrf <ydennekrf@gmail.com>
2022-07-24 11:59:10 -0400

    touched up nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ import aboutBtn from '../assets/svg/amout-btn.svg';
 function NavBar({currentPage, handlePageChange}) {
   return (
     <div className='nav'>
-        <ul >
+        <ul className='nav-list'>
                     <li className='landing-icon'><a href="#" onClick={() => handlePageChange("Home")}><br/><br/><span className={currentPage === 'Home' ? 'nav-link-active' : 'nav-word'}><img src={homeBtn}/>home</span></a></li>
                     <li className='landing-icon'><a href="#" onClick={() => handlePageChange("About")}><br/><br/><span className={currentPage === 'About' ? 'nav-link-active' : 'nav-word'}><img src={aboutBtn}/>about</span></a></li>
                     <li className='landing-icon'><a href="#" onClick={() => handlePageChange("Work")}><br/><br/><span className={currentPage === 'Work' ? 'nav-link-active' : 'nav-word'}><img src={workBtn}/>work</span></a></li>
