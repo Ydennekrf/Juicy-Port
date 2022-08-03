e034d7103d81bf164dfa94689b685a0a210d2361
ydennekrf <ydennekrf@gmail.com>
2022-08-03 08:31:21 -0400

    switch html gen with P3

--- src/components/pages/sub-pages/Work2.js
+++ src/components/pages/sub-pages/Work2.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import HTMLGen from "./screenshots/htmlgen.png";
+import HTMLGen from "./screenshots/Logo.png";
 function Work2() {
   return (
     <div className='workbox'>
         <div className="work-header">
-        <div className="work-link"><a href="https://github.com/Ydennekrf/Html-generator" target="_blank">Git Hub Link</a></div>
-        <div className="title"><h1>Htm.L Gener.Ator.</h1></div>
+        <div className="work-link"><a href="https://stockerinventory.herokuapp.com/" target="_blank">Deployed App</a></div>
+        <div className="title"><h1>Stoc.ker: inventory. App</h1></div>
         </div>
         <div className="work-content">
         <div className="screenshot"><img src={HTMLGen} alt="HTML"/></div>
         <div className="blurb">
         <span className="focus-work">Role:</span><br/>
-          <p>Solo project</p><br/>
+          <p>server side queries and mutations, front end logic for admin page</p><br/>
           <span className="focus-work">Tech:</span><br/>
-          <p>Node.js, JavaScript, HTML, CSS</p><br/>
+          <p>MERN full stack  JWT authentication  graphQL  Apollo Server</p><br/>
           <span className="focus-work">Description:</span><br/>
-          <p>Terminal based app that takes in user data and generates an HTML document</p><br/>
+          <p>A client facing Ecommerce website with an inventory management Admin page that easily accepts an CSV file and can be up and running with little set up.</p><br/>
         </div>
         </div>
         
