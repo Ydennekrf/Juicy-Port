8f1f4a2ab5def2074859aa40e052199ae8ad9e99
ydennekrf <ydennekrf@gmail.com>
2022-08-05 19:01:58 -0400

    fixed linkden link

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -10,7 +10,7 @@ function Socials() {
     <div className="socials">
             <ul className='social-list'>
                 <li className ="socialIcon"><a href="https://www.facebook.com/fraser.kennedy/"><img alt="facebook" src={facebook}/></a></li>
-                <li className ="socialIcon"><a href="www.linkedin.com/in/ydennekrf"><img alt="lindin" src={linkdin}/></a></li>
+                <li className ="socialIcon"><a href="https://www.linkedin.com/in/ydennekrf"><img alt="lindin" src={linkdin}/></a></li>
                 <li className ="socialIcon"><a href="https://www.instagram.com/ydennekrf/"><img alt="instagram" src={instagram}/></a></li>
                 <li className ="socialIcon"><a href="https://github.com/Ydennekrf"><img alt="github" src={github}/></a></li>
                 <li className ="socialIcon"><a href="https://twitter.com/Ydennekrf"><img alt="twitter" src={twitter}/></a></li>
