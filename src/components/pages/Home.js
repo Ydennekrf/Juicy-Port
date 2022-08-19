05a1060a0e7cb44bc686ce50641ee1f800f51823
fraser kennedy <ydennekrf@gmail.com>
2022-08-19 13:09:05 -0400

    added more tweeks

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function Home() {    
     return(
-            <div className="brand">
-                <p><br/>Work Ethic<br/><br/>Problem Solving<br/><br/>Team Management<br/><br/>Attention to Detail<br/><br/></p>
+            <div>
+                
             </div>
        )
 }
