928eebeb1a038df89d5b391098bfde52a7b975a9
fraser kennedy <ydennekrf@gmail.com>
2022-08-19 12:21:36 -0400

    fixing typos

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,8 @@ function About() {
   return (      
          <div className="about">
                 <p><span className="focus">Back-end focused</span> web developer using my food and beverage background to focus on efficiency and data flow. <br/>
-                Earned my <span className="focus">certificate</span> in full stack web development from University of Torontos school of continuing studies, <br/>
-                where I've been able to complement my self learning  with skills in <span className="focus">JavaScript, node.js, and React.</span> <br/>
+                Earned my <span className="focus">certificate</span> in full stack web development from University of Toronto's school of continuing studies, <br/>
+                where I've been able to complement my self-learning  with skills in <span className="focus">JavaScript, node.js, and React.</span> <br/>
                 Known for being a <span className="focus">fast learner</span> with a <span className="focus">strong work ethic</span>, which has developed into a high standard for <span className="focus">efficient and clean </span>code. <br/>
                 Every project I am involved with my aim is always to develop code with the <span className="focus">future</span> in mind, allowing for ease of future development. <br/>
                 I am eager to test my <span className="focus">skills</span> and expand my abilities within a <span className="focus">quality</span> first team.</p>
